fde800233c18269a28cfab958c12db6db35d3d78
Almonier1 <almonier1@gmail.com>
2026-01-31 00:15:34 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -84,4 +84,5 @@ def convert_file():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, use_reloader=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
